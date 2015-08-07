7a0dffddcbc3ce502e5adb6cf819ba81adac176f
youjin9209 <youjin9200@naver.com>
2015-08-08 01:11:19 +0900

    Update upload.php
    final upload.php

--- upload.php
+++ upload.php
@@ -2,20 +2,23 @@
     $videoId = $_POST['videoId'];
     $userId = $_POST['userId'];
     $videoDangerPoint = $_POST['videoDangerPoint'];
-    $videoDate = $_POST['videoDate'];
     $videoReason = $_POST['videoReason'];
     $visitorFace = $_FILES['vistiorFace']['name'];
-    $content = $_FILES['videofile']['name'];
+    //$content = $_FILES['videofile']['name'];
+    $date = new DateTime;
+    $content = $date->format("y_m_d_h_i_s").".webm";
+    $videoDate = $date ->format("y.m.d/h:i");
     
+    $uploaddir='/var/www/html/upload/video/';
+    //$uploadfile = $uploaddir.basename($_FILES['videofile']['name']);
+    $uploadfile = $uploaddir.$content;
     
-    // video 지정한 경로에 저장
-    $uploaddir = '/var/www/html/upload/video/';
-    $uploadfile = $uploaddir.basename($_FILES['videofile']['name']);
+    //잘 들어왔는지 확인하기위해 writetest에 써준다 
     $fp = fopen("writetest2.dat","w");
     
     if(move_uploaded_file($_FILES['videofile']['tmp_name'],$uploadfile)){
       fwrite($fp,"upload success");
-      echo 'video upload success';
+      echo '1';
     }
     fwrite($fp, $_FILES['videofile']['error']);
     
@@ -32,8 +35,7 @@
     
     //날라온거 db에 insert 해줘
     $sql = "INSERT INTO video (userId, videoDangerPoint, videoDate, videoReason, vistorFace, content)
-    VALUES ('$userId','$videoDangerPoint','$videoDate','$videoReason','/var/www/uploads/image/$visitorFace',
-    '/var/www/uploads/video/$content')";
+    VALUES ('$userId','$videoDangerPoint','$videoDate','$videoReason',$visitorFace', '$content')";
     
     fwrite($fp,$sql);
     $result = mysqli_query($link,$sql) or die (fwrite($fp,'die'));
